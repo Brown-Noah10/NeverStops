2d88a104bce148cfc7ede5708ec82fbf16995d34
{Noah <Noah.T.Brown-1@ou.edu>
2020-10-21 17:00:50 -0500

    Test the test

---
+++ src/SuperStackTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SuperStackTest {
+
+	@Test
+	void testIsEmpty() {
+		SuperStack<Integer> stack = new SuperStack<>(1);
+		boolean result = stack.isEmpty();
+		assertTrue(result);
+
+		stack.push(new Integer(0));
+		result = stack.isEmpty();
+		assertFalse(result);
+	}
+
+	@Test
+	void testIsFull() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPush() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
